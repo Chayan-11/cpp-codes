bd71448173dfcf4129ca8a3f8fa7d5f939255675
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-15 00:05:10 +0530

    Update counting no of vowels.cpp

--- counting no of vowels.cpp
+++ counting no of vowels.cpp
@@ -1,3 +1,5 @@
+// Program to find out the total vowels in a string
+
 #include<iostream>
 using namespace std;
 int main()
@@ -26,3 +28,15 @@ int main()
 	}
 	cout << "The total number of vowels present are:- " << counter << endl;
 }
+
+
+/* Algorithm used:- 
+
+1) Ask the user to enter a string.
+2) Read and store the string in a variable.
+3) Iterate through the characters of the string one by one.
+4) Initialize one variable as 0 to store the total count of vowels.
+5) For each character of the string, check if it vowel or not.
+6) If it is a vowel, increment the count variable by 1.
+7) Finally, print out the count variable to the user.
+*/
