b08baf95b8ab7d5a6a57ad2504080c0e82a49926
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-16 00:13:14 +0530

    Update checking for prime number.cpp

--- checking for prime number.cpp
+++ checking for prime number.cpp
@@ -36,12 +36,11 @@ int main()
 
 1) This program takes a positive integer from the user and stores it in the variable n.
 
-Notice that the boolean variable isPrime is initialized to true at the beginning of the program.
+2) The boolean variable isPrime is initialized to true at the beginning of the program.
 
-Since 0 and 1 are not prime numbers, we first check if the input number is one of those numbers or not. If the input number is either 0 or 1, then the value of isPrime is set to false.
+3) Since 0 and 1 are not prime numbers so if the input number is either 0 or 1, then the value of isPrime is set to false.
 
-Else, the initial value of isPrime is left unchanged and the for loop is executed, which checks whether the number entered by the user is perfectly divisible by i or not.
-
+4) Else, the initial value of isPrime is left unchanged and the for loop is executed, which checks whether the number entered by the user is perfectly divisible by i or not.
 
 for (i = 2; i <= n / 2; ++i) {
     if (n % i == 0) {
@@ -49,12 +48,13 @@ for (i = 2; i <= n / 2; ++i) {
         break;
     }
 }
-The for loop runs from i == 2 to i <= n / 2 and increases the value of i by 1 with each iteration.
 
-The loop terminates at i == n / 2 because we cannot find any factor for n beyond the number n / 2. So, any iterations beyond n / 2 is redundant.
+5) The for loop runs from i == 2 to i <= n / 2 and increases the value of i by 1 with each iteration.
 
-If the number entered by the user is perfectly divisible by i, then isPrime is set to false and the number will not be a prime number.
+6) The loop terminates at i == n / 2 because we cannot find any factor for n beyond the number n / 2. So, any iterations beyond n / 2 is redundant.
 
-But if the input number is not perfectly divisible by i throughout the entirety of the loop, then it means that the input number is only divisible by 1 and that number itself.
+7) If the number entered by the user is perfectly divisible by i, then isPrime is set to false and the number will not be a prime number.
+
+8) But if the input number is not perfectly divisible by i throughout the entirety of the loop, then the input number is only divisible by 1 and that number itself.
 
 So, the given number is a prime number.*/
