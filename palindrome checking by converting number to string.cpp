10ccd3f17423d175a994cead59b8569640fc698b
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-26 00:45:31 +0530

    Update palindrome checking by converting number to string.cpp

--- palindrome checking by converting number to string.cpp
+++ palindrome checking by converting number to string.cpp
@@ -31,3 +31,40 @@ int main()
 	}
 	return 0;
 }
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	
+// Program to check whether a number is palindrome or not using normal method of reversing a number and comparing with original number
+
+#include<iostream>
+using namespace std; 
+
+int getReverseNumber(int n)
+{
+	int reverse = 0;
+	int right_most_digit;
+	
+	while(n != 0)
+	{
+		right_most_digit = n%10;
+		reverse = (reverse*10) + right_most_digit;
+		n = n/10; 
+	}
+	return reverse;
+} 
+int main()
+{
+	int n; 
+	cout << "Enter a number:- " << endl;
+	cin >> n;
+	if(n == getReverseNumber(n))
+	{
+		cout << n << " is a palindrome number" << endl;
+	}
+	else
+	{
+		cout << n << " is not a palindrome number" << endl;
+	}
+	return 0;
+}
