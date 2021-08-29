73dfe55b816de5b68d4aeef3a5eeab0a08d436df
Chayan Debbarma <chayan.nit24@gmail.com>
2021-08-30 00:02:48 +0530

    Update fdim header file illustration.cpp

--- fdim header file illustration.cpp
+++ fdim header file illustration.cpp
@@ -14,11 +14,18 @@ int main ()
 	return 0;
 }
 
+
 /* NOTE:- 
 
-fdim, fdimf and fdiml functions are defined in the cmath header. These functions are used to find the difference between two numbers.
+#) fdim, fdimf and fdiml functions are defined in the cmath header. These functions are used to find the difference between two numbers.
+
+#) syntax and declarations:- 
+
 double fdim(double num1, double num2)
 float fdimf(float num1, float num2)
 long double fdiml(long double num1, long double num2)
-As you can see, all these functions take two parameters. The return value is double, float or long double. It returns the value of num1 - num2 if num1 > num2. Else, it returns zero.
+
+All these functions take two parameters.
+The return value is double, float or long double. It returns the value of num1 - num2 if num1 > num2. Else, it returns zero.
+
 */
